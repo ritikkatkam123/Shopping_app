018d67652aaec92d33708054a1663ecc92c76865
ritikkatkam123 <ritik.katkam@colostate.edu>
2025-11-08 16:49:57 -0700

    shopping app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import ShoeList from "./components/ShoeList";
+import Cart from "./components/Cart";
+import shoesData from "./data/ShoeData";
+import "./App.css";
 
 function App() {
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem("cart");
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  const addToCart = (shoe) => {
+    const existing = cart.find((item) => item.id === shoe.id);
+    if (existing) {
+      setCart(
+        cart.map((item) =>
+          item.id === shoe.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      );
+    } else {
+      setCart([...cart, { ...shoe, quantity: 1 }]);
+    }
+  };
+
+ 
+  const removeFromCart = (id) => {
+    setCart((prevCart) =>
+      prevCart
+        .map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
+
+  const removeAll = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ShoeList shoes={shoesData} addToCart={addToCart} />
+      <Cart cart={cart} removeFromCart={removeFromCart} removeAll={removeAll} />
     </div>
   );
 }
-
 export default App;
